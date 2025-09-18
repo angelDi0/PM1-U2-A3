dc59061c9a4614e61511de5bcaf9b87d8cf4f6da
angelDi0 <angel.diosdado1239@gmail.com>
2025-09-18 13:14:31 -0600

    6.- Integracion de un text que se muestra en el incremento del contador del vaso de agua y se borra si se presiona el boton de Close

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -14,33 +14,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.saveable.rememberSaveable
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier){
-    Column (modifier = modifier.padding(16.dp)) {
-//        val count: MutableState<Int> = remember {mutableStateOf(0)}
-        var count by remember { mutableStateOf(0)}
-
-        if(count > 0){
-            var showTask by remember { mutableStateOf(true)}
-            if(showTask){
-                WellnessTaskItem(
-                    onClose = { showTask = false},
-                    taskName = "Has caminado los 15 minutos diarios"
-                )
-            }
-            Text("Te has tomado $count vasos de agua")
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by rememberSaveable { mutableStateOf(0) }
+        if (count > 0) {
+            Text("You've had $count glasses.")
         }
-        Row(Modifier.padding(top = 8.dp)){
-            Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10){
-                Text("Agregar uno")
-            }
-            Button(
-                onClick = { count = 0},
-                Modifier.padding(start = 8.dp)
-            ){
-                Text("Reiniciar contador de vasos de agua?")
-            }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
         }
     }
 }
